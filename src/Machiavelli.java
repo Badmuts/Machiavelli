250c913fc152d1c6c89a1cef637223b5fc9effa9
Daan Rosbergen <d.rosbergen@gmail.com>
2015-05-28 11:39:12 +0200

    Add bootstrap file

---
+++ src/Machiavelli.java
@@ -0,0 +1,10 @@
+/**
+ * Created by daanrosbergen on 28/05/15.
+ */
+public class Machiavelli {
+
+    public static void main(String[] args) {
+        System.out.println("Machiavelli");
+    }
+
+}
