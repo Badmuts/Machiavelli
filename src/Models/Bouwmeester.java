ab913cf607d3cfec636f04fbce8303da397f856e
Daan Rosbergen <d.rosbergen@gmail.com>
2015-06-03 19:42:48 +0200

    Implement KarakterInterface

--- src/Models/Bouwmeester.java
+++ src/Models/Bouwmeester.java
@@ -1,7 +1,13 @@
 package Models;
 
+import Interfaces.Karakter;
+
 /**
  * Created by daanrosbergen on 03/06/15.
  */
-public class Bouwmeester {
+public class Bouwmeester implements Karakter {
+    @Override
+    public void gebruikEigenschap() {
+        // TODO: bouwen 3 gebouwen
+    }
 }
