fecf0818751a509c2c69217efe7760fc8a1da545
Sander de Jong <s1088313@student.hsleiden.nl>
2015-06-03 21:02:52 +0200

    Opzet inkomstencontroller + bijbehorende models

---
+++ src/Models/Bank.java
@@ -0,0 +1,21 @@
+package Models;
+/**
+ * 
+ * @author Sander de Jong
+ *
+ */
+public class Bank
+{
+	// Variables
+	private short goudMunten;
+	
+	public void ontvangenGoud()
+	{
+		
+	}
+	
+	public void gevenGoud()
+	{
+		
+	}
+}
---
+++ src/Models/GebouwKaart.java
@@ -0,0 +1,15 @@
+package Models;
+
+import Enums.GebouwTypes;
+
+/**
+ * 
+ * @author Sander
+ *
+ */
+public class GebouwKaart
+{
+	private short kosten;
+	private String naam;
+	private GebouwTypes type;
+}
