21bd35424d0deeb5de499fcdb920ebf60bce1649
Daan Rosbergen <d.rosbergen@gmail.com>
2015-06-03 21:31:55 +0200

    Add attributes to Moordenaar and add method setVermoordKarakter

--- src/Models/Moordenaar.java
+++ src/Models/Moordenaar.java
@@ -1,13 +1,25 @@
 package Models;
 
+import Enummerations.Type;
 import Interfaces.Karakter;
 
 /**
  * Created by daanrosbergen on 03/06/15.
  */
 public class Moordenaar implements Karakter {
+
+    private String      naam                = "Moordenaar";
+    private int         nummer              = 1;
+    private int         bouwLimiet          = 1;
+    private Type        type                = Type.NORMAAL;
+    private Karakter    vermoordKarakter    = null;
+
     @Override
     public void gebruikEigenschap() {
         // TODO: vermoord karakter
+    }
+
+    public void setVermoordKarakter(Karakter karakter) {
+        this.vermoordKarakter = karakter;
     }
 }
