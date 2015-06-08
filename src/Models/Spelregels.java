98b8f4c01b83bb6d1dc750e4156779bb9660c2ce
JamieKalloe <jamie@kalloe.net>
2015-06-08 09:41:36 +0100

    made some changes to Spelregels

--- src/Models/Spelregels.java
+++ src/Models/Spelregels.java
@@ -33,7 +33,7 @@ public class Spelregels {
 	public String getSpelregelsFromResource(String fileName)
 	{
 		String text = null;
-		File file = new File("bin/Resources/spelregels.txt");
+		File file = new File("bin" + File.separator + "Resources" + File.separator + "spelregels.txt");
 		String absolutePath = file.getAbsolutePath();
 		try 
 		{
