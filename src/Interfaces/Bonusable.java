226e85b85da01bb62341874ca575b2acc6c00e11
Daan Rosbergen <d.rosbergen@gmail.com>
2015-06-08 20:40:15 +0200

    Add comment to Bonusable interface

--- src/Interfaces/Bonusable.java
+++ src/Interfaces/Bonusable.java
@@ -1,7 +1,24 @@
 package Interfaces;
 
 /**
- * Created by daanrosbergen on 08/06/15.
+ * Deze interface word gebruikt voor karakters die een
+ * bonus ontvangen voor gebouwen van het zelfde type
+ * als zij zijn.
+ *
+ * Voorbeeld: De koning is van het type monument.
+ * De speler die het karakter koning is heeft
+ * 2 gebouwen van het type monument in zijn stad.
+ *
+ * Voor elk gebouw in zijn hand van hetzelfde type
+ * ontvangt hij 1 goudstuk. In dit geval ontvangt hij
+ * dus 2 goudstukken aan bonus goud.
+ *
+ * Er zijn 4 types die recht hebben op bonus goud:
+ * - MONUMENT
+ * - KERKELIJK
+ * - COMMERCIEL
+ * - MILITAIR
+ *
  */
 public interface Bonusable {
 
