d87e560e31be2161b54bc56107cc694ba194a6ff
Jimmy Nguyen <s1087315@student.hsleiden.nl>
2015-06-27 20:18:37 +0200

    ik ben retarded, excuses. haast hebben is niet al te best voor je code - Albert Newton 500 Vchr

--- src/Machiavelli/Interfaces/Remotes/BeurtRemote.java
+++ src/Machiavelli/Interfaces/Remotes/BeurtRemote.java
@@ -24,8 +24,6 @@ public interface BeurtRemote extends Remote {
   public void setObserverIndex(int observerIndex) throws RemoteException;
 
   public void addObserver(BeurtObserver beurtObserver) throws RemoteException;
-  
-  public ArrayList<BeurtObserver> getBeurtObserver() throws RemoteException;
 
   public void notifyObservers() throws RemoteException;
 
