f30f2db7146dbc5d321e7c5f20aed264bb8d0b1c
Sander de Jong <s1088313@student.hsleiden.nl>
2015-06-29 16:14:59 +0200

    update hervatten spel, close but not close enough

--- src/Machiavelli/Interfaces/Remotes/SpelRemote.java
+++ src/Machiavelli/Interfaces/Remotes/SpelRemote.java
@@ -42,9 +42,9 @@ public interface SpelRemote extends Remote {
 
     void laadSpel(SpelRemote loadSpel) throws RemoteException;
 
-    ArrayList<SpelerRemote> getTempSpelers();
+    ArrayList<SpelerRemote> getTempSpelers() throws RemoteException;
 
-    void setTempSpelers(ArrayList<SpelerRemote> list);
+    void setTempSpelers(ArrayList<SpelerRemote> list) throws RemoteException;
 
     ArrayList<SpelObserver> getObservers() throws RemoteException;
 
