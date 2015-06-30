7b5c3c2af91d07c1f7226f32cb1c514d1e846258
Daan Rosbergen <d.rosbergen@gmail.com>
2015-06-30 09:48:45 +0200

    Move RMI registry start Sysout to top

--- src/server/Server.java
+++ src/server/Server.java
@@ -23,11 +23,11 @@ public class Server {
         try {
             // Hardcode ipadres van server, zet deze uit voor lokale tests.
             System.setProperty("java.rmi.server.hostname", "145.97.16.203"); // Hardcode ipadres van server
+            System.out.println("RMI Registry starter");
             Registry registry = LocateRegistry.createRegistry(1099); // default port 1099 // run RMI registry on local host
             Spel spel = new Spel();
             // Maak van spel een skeleton
             SpelRemote spelSkeleton = (SpelRemote) UnicastRemoteObject.exportObject(spel, 1099);
-            System.out.println("RMI Registry starter");
             registry.rebind("Spel", spelSkeleton); // bind Spel to RMI registry
             System.out.println("Spel skeleton bound");
             System.out.println("Server running...");
