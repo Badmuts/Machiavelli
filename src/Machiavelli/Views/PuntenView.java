b2dcf5c53f7e9c49d5ebd513ab58a154d3249bc8
Jimmy Nguyen <s1087315@student.hsleiden.nl>
2015-06-30 05:46:53 +0200

    puntenview klasse aangemaakt (applicatiemodel)

---
+++ src/Machiavelli/Views/PuntenView.java
@@ -0,0 +1,12 @@
+package Machiavelli.Views;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class PuntenView extends UnicastRemoteObject {
+
+    public PuntenView() throws RemoteException {
+
+    }
+
+}
