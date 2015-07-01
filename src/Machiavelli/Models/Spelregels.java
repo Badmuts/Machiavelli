90279e66e6e5b7fdd6712d1385d7c09262ea7255
Jimmy Nguyen <s1087315@student.hsleiden.nl>
2015-07-01 19:54:53 +0200

    Fix spelregels voor jar file

--- src/Machiavelli/Models/Spelregels.java
+++ src/Machiavelli/Models/Spelregels.java
@@ -1,77 +1,69 @@
 package Machiavelli.Models;
 
-import Machiavelli.Interfaces.Observers.SpelregelsObserver;
-import Machiavelli.Interfaces.Remotes.SpelregelsRemote;
-
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import Machiavelli.Machiavelli;
+import Machiavelli.Interfaces.Observers.SpelregelsObserver;
+import Machiavelli.Interfaces.Remotes.SpelregelsRemote;
+
 /**
  * @author Jamie Kalloe
  * 
-<<<<<<< HEAD
- *         Het Spelregels model wordt gebruikt om de spelregels mee op te halen voor de view.
+ *         <<<<<<< HEAD Het Spelregels model wordt gebruikt om de spelregels mee op te halen voor de
+ *         view.
  * 
-=======
- * De spelregels van Machiavelli zijn opgeslagen in een textdocument, deze
- * kunnen op elk moment in het spel aangeroepen worden.
+ *         ======= De spelregels van Machiavelli zijn opgeslagen in een textdocument, deze kunnen op
+ *         elk moment in het spel aangeroepen worden.
  *
->>>>>>> 6395149309661da4f9c5b5c341ca69de3c13d1c0
+ *         >>>>>>> 6395149309661da4f9c5b5c341ca69de3c13d1c0
  */
 
 public class Spelregels implements SpelregelsRemote, Serializable {
-  private ArrayList<SpelregelsObserver> observers = new ArrayList<>();
+    private ArrayList<SpelregelsObserver> observers = new ArrayList<>();
 
-  /**
-   * Retourneerd de spelregels die zijn opgehaald uit de text file.
-   * 
-   * @return spelregels String
-   */
-  public String getSpelregels() throws IOException {
-    return this.getSpelregelsFromResource("spelregels.txt");
-  }
-
-  /**
-   * Deze method haalt de spelregels uit een textfile van het opgegeven pad.
-   * 
-   * @param fileName naam de van de file waar de spelregels in staan.
-   */
-  private String getSpelregelsFromResource(String fileName) {
-    String text = null;
-
-    File file =
-        new File("src" + File.separator + "Machiavelli/Resources" + File.separator
-            + "spelregels.txt");
-
-    String absolutePath = file.getAbsolutePath();
-    try {
-      text = new Scanner(new File(absolutePath), "UTF-8").useDelimiter("\\A").next();
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
+    /**
+     * Retourneerd de spelregels die zijn opgehaald uit de text file.
+     * 
+     * @return spelregels String
+     */
+    public String getSpelregels() throws IOException {
+        return this.getSpelregelsFromResource("spelregels.txt");
     }
 
-    return text;
-  }
+    /**
+     * Deze method haalt de spelregels uit een textfile van het opgegeven pad.
+     * 
+     * @param fileName naam de van de file waar de spelregels in staan.
+     */
+    private String getSpelregelsFromResource(String fileName) {
 
-  /**
-   * Voert de SpelregelsObserver toe aan de lijst met observers.
-   */
-  public void addObserver(SpelregelsObserver observer) throws RemoteException {
-    observers.add(observer);
-  }
+        Scanner scanner =
+                new Scanner(Machiavelli.class.getResourceAsStream("Resources/spelregels.txt"),
+                        "UTF-8");
+        String text = scanner.useDelimiter("\\A").next();
+        scanner.close();
+        return text;
 
-  /**
-   * Vertelt de observers dat het model is veranderd.
-   */
-  public void notifyObservers() throws RemoteException {
-    for (SpelregelsObserver observer : observers) {
-      observer.modelChanged(this);
     }
-  }
+
+    /**
+     * Voert de SpelregelsObserver toe aan de lijst met observers.
+     */
+    public void addObserver(SpelregelsObserver observer) throws RemoteException {
+        observers.add(observer);
+    }
+
+    /**
+     * Vertelt de observers dat het model is veranderd.
+     */
+    public void notifyObservers() throws RemoteException {
+        for (SpelregelsObserver observer : observers) {
+            observer.modelChanged(this);
+        }
+    }
 
 }
