4958072b59e52fde4ee4c15a9845163559ea10a9
Jimmy Nguyen <s1087315@student.hsleiden.nl>
2015-07-03 09:52:58 +0200

    puntenview geadd

---
+++ src/Machiavelli/Controllers/PuntenController.java
@@ -0,0 +1,13 @@
+package Machiavelli.Controllers;
+
+import java.rmi.RemoteException;
+
+import Machiavelli.Views.PuntenView;
+
+public class PuntenController {
+
+    public PuntenController() throws RemoteException {
+        PuntenView puntenView = new PuntenView();
+        
+    }
+}
--- src/Machiavelli/Views/PuntenView.java
+++ src/Machiavelli/Views/PuntenView.java
@@ -3,10 +3,13 @@ package Machiavelli.Views;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import javafx.scene.text.Text;
+
 public class PuntenView extends UnicastRemoteObject {
 
     public PuntenView() throws RemoteException {
-
+        private Text title;
+        private 
     }
 
 }
